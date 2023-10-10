9086b01599f92498b507bf624eda385353bb1b7e
Mahmoud Ghanem <moghanem345@gmail.com>
2023-10-10 12:50:58 +0300

    set up the app

---
+++ index.js
@@ -0,0 +1,16 @@
+// create a simple application 
+const express = require('express');
+
+// initialize express
+const PORT = process.env.PORT || 2000;
+const app = express();
+
+// create a route
+app.get('/', (req, res) => {
+    res.send('Hello Docker');
+});
+
+// listen on port 3000
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+});

caab9161c36ddfb29eb4961215a7aa751744f0d5
Mahmoud Ghanem <moghanem345@gmail.com>
2023-10-13 12:35:21 +0300

    connect docker with mongoDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 // create a simple application 
 const express = require('express');
+const mongoose = require('mongoose');
 
 // initialize express
 const PORT = process.env.PORT || 2000;
 const app = express();
+
+// connect to mongodb
+const DB_USER = root;
+const DB_PASSWORD = example;
+const DB_PORT = 27017;
+const DB_HOST = 'mongo';
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // create a route
 app.get('/', (req, res) => {
